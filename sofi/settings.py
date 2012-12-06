e1bfd0793808543250b29cb86b5118e5a2eabd8f
Ann Kpx <annkpx@gmail.com>
2012-12-06 12:03:22 +0400

    settings fix

--- sofi/settings.py
+++ sofi/settings.py
@@ -21,10 +21,11 @@ DATABASES = {
         'NAME': 'sofi',   # Or path to database file if using sqlite3.
         'USER': 'sofi',                      # Not used with sqlite3.
         'PASSWORD': 'sofi',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
